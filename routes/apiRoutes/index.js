84ff0e3bbc8bb941081705ee65df2f3d13b9a4d6
kenng8891 <103011617+kenng8891@users.noreply.github.com>
2022-07-04 22:26:59 -0400

    added votes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./voterRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 // Use apiRoutes
 app.use('/api', apiRoutes);
 
@@ -16,7 +17,11 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
